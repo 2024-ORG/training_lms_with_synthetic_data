85e70d49c383c1abd49549c5dd9ab63b7b3ed2f9
devops-vulcan <92709040+devops-vulcan@users.noreply.github.com>
2025-01-06 10:11:26 +0200

    Create missing_sc.js

---
+++ missing_sc.js
@@ -0,0 +1,17 @@
+let x = 5
+
+if (condition) {
+// Missing indentation
+doSomething(); 
+}
+
+console.log("Hello");;
+
+if (x === x) { 
+    // This condition is always true
+}
+
+if (false) {
+    // This code will never execute
+    doSomething(); 
+}
